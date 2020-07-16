95728dfcd75acb0db3eb195aa753af11c70e134c
丁盾 <dun_ding@fpi-inc.com>
2020-07-16 10:15:43 +0800

    feat: IM组件

--- src/view/Firend/firend.js
+++ src/view/Firend/firend.js
@@ -1,13 +1,42 @@
-import React, {useEffect} from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 import {Text, View} from 'react-native';
 
-export default function HomeScreen() {
+export default function FirendScreen() {
+    const [messages, setMessages] = useState([])
+    const [ddendumTime, setAddendumTime] = useState(60)
+
     useEffect(() => {
-        console.log('xxxxx');
+        const newMessage = [{
+            _id: ddendumTime,
+            text: `Hello developer${ddendumTime}`,
+            createdAt: new Date(),
+            user: {
+                _id: ddendumTime,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        }]
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessage))
+        let timer = setTimeout(() => {
+            setAddendumTime(prevCount => prevCount - 1)
+        }, 3000)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [ddendumTime])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
     }, [])
+
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>im!</Text>
-        </View>
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)} 
+            user={{
+                _id: 1,
+            }}
+        />
     )
 } 
